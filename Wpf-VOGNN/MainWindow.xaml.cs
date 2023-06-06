b2b21846721b90db0a0c5e7799874af929a655ad
Julia Mamsheva <Jukudryash@gmail.com>
2023-06-06 20:03:48 +0300

    Исправлена ошибка login

--- Wpf-VOGNN/MainWindow.xaml.cs
+++ Wpf-VOGNN/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Wpf_VOGNN
 {
@@ -47,7 +33,7 @@ namespace Wpf_VOGNN
         {
             if (pages == pages.login)//если форма открытия логин
             {
-                frame.Navigate(new login(this));//открываем форму логин
+                frame.Navigate(new LoginUser(this));//открываем форму логин
             }
             else if (pages == pages.regin)
             {
