98d9d3de6e254e492f83e02b5c49d6629b09bb1f
AlexeyLikhodeev <alesha.likhodeev@yandex.ru>
2023-07-02 20:28:55 +0300

    окно созд карточки

--- Wpf-VOGNN/Windows/AddUchCardWindow.xaml.cs
+++ Wpf-VOGNN/Windows/AddUchCardWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace Wpf_VOGNN.Windows
             PrintDialog pd = new PrintDialog();
             if (pd.ShowDialog() == true)
             {
-                pd.PrintVisual(RegtPrint, "Печать");
+                pd.PrintVisual(GridPrint, "Печать");
             }
         }
     }
